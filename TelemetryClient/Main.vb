ba2f223aa6f70d91888742144eea055a9b7b950a
Nathan Lindquist <nallink@comcast.net>
2016-02-18 21:30:14 -0600

    Created server/client stubs.

---
+++ TelemetryClient/Main.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Net.Sockets
+
+Module Main
+    Sub Main()
+        Dim addr As String = "127.0.0.1"
+        Dim port As Int32 = 2000
+        Dim message As String = "Hello server!"
+        Dim data As Byte() = System.Text.Encoding.ASCII.GetBytes(message)
+        Dim client As TcpClient = New TcpClient(addr, port)
+        Console.WriteLine("Connected.")
+
+        Dim stream As NetworkStream = client.GetStream()
+        stream.Write(data, 0, data.Length)
+        Console.WriteLine("Sent {0}", message)
+
+        stream.Close()
+        client.Close()
+        Console.WriteLine("Closed.")
+
+        Console.Write("Press enter to continue...")
+        Console.Read()
+    End Sub
+End Module
