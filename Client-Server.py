e900cfc7f1aab484568dc45afaea4d6b15c5e5dd
Kirby Julin <kirbyjulian2021@u.northwestern.edu>
2018-04-28 13:45:39 -0500

    Deleted sc7 telemetry code and Added Client-Server interactions
    To begin creating the new telemetry system in Python, Ethan Park and Kirby Julian deleted all current sc7 telemetry files and uploaded the test for working Client-Server interactions in Python

---
+++ Client-Server.py
@@ -0,0 +1,12 @@
+# Telemetry System
+# April 28, 2018
+import socket
+
+HOST = '165.124.144.223'
+PORT = 50007
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+    print('Received', repr(data))
--- README.md
+++
@@ -1,4 +0,0 @@
-# sc7-telemetry-code
-
-Google Document for CanTAGs:
- https://docs.google.com/document/d/1KHb4gFU-01AxH5zbdxCr0NWuHKqFl5ZvuHHw04qlHKc/edit
